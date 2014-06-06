7024e861057dcbff28d09d2f622ccbb21c974c87
plhwin <plhwin@plhwin.com>
2014-06-06 13:18:57 +0800

    fix 'width bug' in index.html

--- client/client.js
+++ client/client.js
@@ -85,7 +85,10 @@
 			return false;
 		},
 		init:function(username){
-			//客户端根据时间和随机数生成uid,这样使得聊天室用户名称可以重复。实际项目中，如果是需要用户登录，那么直接采用用户的uid来做标识就可以
+			/*
+			客户端根据时间和随机数生成uid,这样使得聊天室用户名称可以重复。
+			实际项目中，如果是需要用户登录，那么直接采用用户的uid来做标识就可以
+			*/
 			this.userid = this.genUid();
 			this.username = username;
 			
