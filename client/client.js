6232c7ac796fa55cac0902c80f7f9cdb9549fa1b
Peter Pan <plhwin@plhwin.com>
2016-05-23 20:41:29 +0800

    remove port 3000

--- client/client.js
+++ client/client.js
@@ -97,7 +97,7 @@
 			this.scrollToBottom();
 			
 			//连接websocket后端服务器
-			this.socket = io.connect('ws://realtime.plhwin.com:3000');
+			this.socket = io.connect('ws://realtime.plhwin.com');
 			
 			//告诉服务器端有用户登录
 			this.socket.emit('login', {userid:this.userid, username:this.username});
